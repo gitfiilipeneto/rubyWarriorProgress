a489ddfeccba4bbb4d353caa3f7524f5c3afdf7c
Filipe <filipenetopires@hotmail.com>
2021-04-21 18:18:33 -0300

    proceed to lv 4

--- rubywarrior/lipe-beginner/player.rb
+++ rubywarrior/lipe-beginner/player.rb
@@ -1,11 +1,17 @@
 class Player
+  
   def play_turn(warrior)
-    # add your code 
-    if warrior.feel.empty?
-      warrior.walk!
-    else warrior.attack!
-      end
+    # add your code
+    if warrior.feel.enemy?
+      warrior.attack!
+
+    elsif warrior.feel.empty? and warrior.health < 12
+      warrior.rest!
+      
+    
+    else warrior.walk!
+    end
 
   end
-
+  
 end
